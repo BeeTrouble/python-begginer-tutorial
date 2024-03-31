fa5406a424b455fb03b0d7241e4207bd7c9b9fc2
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 11:35:19 +0300

    RPS - Refactoring and Nested If
    RPS - Refactoring and Nested If

--- main.py
+++ main.py
@@ -12,9 +12,23 @@ def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}")
     if player == computer:
         return "It'S a tie!"
-    elif player == "rock" and computer == "scissors":
-        return "Rock smashes scissors! You Win!"
-    elif player == "rock" and computer == "paper":
-        return "Paper covers rock! You Lose!"
+    
+    elif player == "rock":  
+        if computer == "scissors":
+            return "Rock smashes scissors! You Win!"
+        else:
+            return "Paper covers rock! You Lose."
+        
+    elif player == "paper":  
+        if computer == "rock":
+            return "Paper covers rock! You Win!"
+        else:
+            return "Scissors cuts paper! You Lose."
+        
+    elif player == "scissors":  
+        if computer == "paper":
+            return "Scissors cuts paper! You Win!"
+        else:
+            return "Rock smashes scissors! You Lose."
 
-check_win("rock", "paper")
+choises = get_choices()
