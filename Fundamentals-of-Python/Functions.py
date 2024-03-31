970f305ce361cd234ae187f003b60d4a188fc671
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 19:13:30 +0300

    Nested Functions
    Nested Functions

--- Fundamentals-of-Python/Functions.py
+++ Fundamentals-of-Python/Functions.py
@@ -55,3 +55,28 @@ def test():
 
 print(age)
 test()
+
+
+## Nested Functions 
+
+def talk(phrase):
+    def say(word):
+        print(word)
+    
+    words = phrase.split(' ')
+    for word in words:
+        say(word)
+
+talk('I am going to buy the milk')
+
+def count():
+    count = 0
+
+    def  increment():
+        nonlocal count
+        count = count + 1
+        print(count)
+    
+    increment()
+
+count()
