ab45ae2ad77969f1a1725a33540cb1ddab1afd9e
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 16:03:33 +0300

    Lists
    Lists

---
+++ Fundamentals-of-Python/Lists.py
@@ -0,0 +1,42 @@
+## Lists
+
+dogs = ["Roger", 1, "Syd", True, "Quincy", 7]
+
+print("Roger" in dogs)
+print("Ali" in dogs)
+
+print(dogs[0])
+
+dogs[1] = "Pamuk"
+
+print(dogs[1])
+print(dogs[-1])
+print(dogs[2:])
+print(dogs[:3])
+
+print(len(dogs))
+
+dogs.append("Judah")
+print(dogs)
+
+dogs.extend(["Mahmut", 23 , "Kai"])
+print(dogs)
+
+dogs += ["White"]
+print(dogs)
+
+dogs.remove("Syd")
+print(dogs)
+
+print(dogs.pop())
+print(dogs)
+
+
+items = ["Roger" , 1, "Syd", True, "Quincy", 7]
+
+items.insert(2, "Test")
+print(items)
+
+items[1:1] = ["Test1", "Test2"]
+
+print(items)
