04eeaf65c302c912eda11eb3c6b8394a09512dee
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 15:39:27 +0300

    Enums
    Enums

---
+++ Fundamentals-of-Python/Enums.py
@@ -0,0 +1,13 @@
+## Enums
+from enum import Enum
+
+class State(Enum):
+    INACTIVE = 0
+    ACTIVE = 1
+
+print(State.ACTIVE)
+print(State.ACTIVE.value)
+print(State(1))
+print(State['ACTIVE'])
+print(list(State))
+print(len(State))
