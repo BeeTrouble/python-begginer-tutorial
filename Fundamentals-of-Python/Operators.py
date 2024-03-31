50d7e2b4ee6a06e266f14b6b8a2e2edb3ede15b2
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 14:30:02 +0300

    Comparison Operators
    Comparison Operators

--- Fundamentals-of-Python/Operators.py
+++ Fundamentals-of-Python/Operators.py
@@ -1,19 +1,31 @@
 # Operators
+
 age = 20
 
 ##Arithmetic Operators
-1 + 1 #2
-2 - 2 #1
-2 * 2 #4
-4 / 2 #2
-4 % 3 #1
-4 ** 2 #16
-4 // 2 #2
+
+1 + 1  # 2
+2 - 2  # 1
+2 * 2  # 4
+4 / 2  # 2
+4 % 3  # 1
+4 ** 2 # 16
+4 // 2 # 2
 
 age = 12
 age += 89 # age = age + 89 = 101
 print(age)
 
 number = 2000
-number *= 2 # age = age * 2 = 4000
+number *= 2 # number = number * 2 = 4000
 print(number)
+
+## Comparison Operators
+
+a = 1
+b = 2
+
+a == b # False
+a != b # True
+a > b  # False
+a <= b # True
