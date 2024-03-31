cf6284d896a823c8347b1aa03bb00aa547f4c9b7
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 11:42:31 +0300

    RPS - Accessing Dictionary Values
    RPS - Accessing Dictionary Values

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
-    options = ["rock", "paper", "scissor"]
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choises = {"player": player_choice, "computer": computer_choice}
 
@@ -32,3 +32,5 @@ def check_win(player, computer):
             return "Rock smashes scissors! You Lose."
 
 choises = get_choices()
+result = check_win(choises["player"], choises["computer"])
+print(result)
