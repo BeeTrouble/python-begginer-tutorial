3f6d8a52659e88cbc005a26e001bb517ecab05ea
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 14:27:07 +0300

    Arithmetich Operators
    Arithmetich Operators

--- Fundamentals-of-Python/Operators.py
+++ Fundamentals-of-Python/Operators.py
@@ -1,1 +1,19 @@
+# Operators
 age = 20
+
+##Arithmetic Operators
+1 + 1 #2
+2 - 2 #1
+2 * 2 #4
+4 / 2 #2
+4 % 3 #1
+4 ** 2 #16
+4 // 2 #2
+
+age = 12
+age += 89 # age = age + 89 = 101
+print(age)
+
+number = 2000
+number *= 2 # age = age * 2 = 4000
+print(number)
