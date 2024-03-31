bd8ffbd7a9c0c00be40df3842721af1e4fa19e35
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 10:51:59 +0300

    RPS - Dictionaries
    RPS - Dictionaries

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def get_choices():
     player_choice = "rock"
     computer_choice = "paper"
+    choises = {"player": player_choice, "computer": computer_choice}
 
-    return computer_choice
+    return choises
 
 choises = get_choices()
 print(choises)
+
+dict = {"name": "beau", "color": "blue"}
