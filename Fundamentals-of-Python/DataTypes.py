9cef35ee84bbb815428cc03e76b362011e35ce88
BeeTrouble <ciftci.kaan@hotmail.com>
2024-03-31 14:13:44 +0300

    Variables, Expressions and Statements, Comments, Data Types
     Variables, Expressions and Statements, Comments, Data Types

---
+++ Fundamentals-of-Python/DataTypes.py
@@ -0,0 +1,15 @@
+#The date 
+date = "31 March 2024"
+print(isinstance(date, str))
+
+#The number of turkish alphabet
+number = 28
+print(isinstance(number, int))
+
+# 1 mile to kilometer
+mile = 1.61 
+print(isinstance(mile, float))
+
+# Happy or not
+happy = True
+print(isinstance(happy, bool))
