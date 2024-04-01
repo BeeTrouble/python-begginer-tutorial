d96751ee3092bbeda50ec0873792da7ee82f6ec6
BeeTrouble <ciftci.kaan@hotmail.com>
2024-04-01 18:28:42 +0300

    Decorators
    Decorators

---
+++ Fundamentals-of-Python/Decorators.py
@@ -0,0 +1,16 @@
+## Decorators
+
+
+def logtime(func):
+    def wrapper():
+        print("before")
+        val = func()
+        print("after")
+        return val
+    return wrapper
+
+@logtime
+def hello():
+    print("hello")
+
+hello()
