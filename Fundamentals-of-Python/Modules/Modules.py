21ec49d4f0601ccdec19589448e75b4db4c95d95
BeeTrouble <ciftci.kaan@hotmail.com>
2024-04-01 17:36:45 +0300

    Modules
    Modules

---
+++ Fundamentals-of-Python/Modules/Modules.py
@@ -0,0 +1,31 @@
+## Modules
+
+from lib import Dog
+
+Dog.bark()
+
+
+from lib.Dog import bark
+
+bark()
+
+# ' math ' for math utilities
+# ' re ' for regular expressions
+# ' json ' to work with JSON
+# ' datetime ' to work with dates
+# ' sqlite3 ' to use SQLite
+# ' of ' for Operating System utilities
+# ' random ' for random number generation
+# ' statistics ' for statistics utilities
+# ' requests ' to perform HTTP network requests
+# ' http ' to create HTTP servers 
+# ' urllib ' to manage URLs
+
+
+import math
+
+print(math.sqrt(4))
+
+from math import sqrt
+
+print(sqrt(4))
